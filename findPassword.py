f8d8cdb95bd5a1a0cbd0c1d165ea254a589e52a9
Ujjwalsharma22 <ujjwalsharma2210@gmail.com>
2020-11-29 13:18:34 +0530

    added numbers in findpassword.py

--- findPassword.py
+++ findPassword.py
@@ -5,20 +5,19 @@
 #               							  #
 ###############################################
 
+# ISSUES
+# repeating elements give problem
+# Program stops printing but resumes at some point again (I don't know why)
+
 from itertools import permutations
-from collections import Counter
+# from collections import Counter   # Was used in findThisPass() to check equality of lists
 import functools
 # Added delay to make it look cooler ;)
 import time
-# while checked != password:
-#     for i in range(0, int(len(password))):
-#         for j in range(0, int(len(password))):
-#             checked += characters[i]
-#
-#     print(checked)
 
 # This function is seperated because you can add more characters
 def charactersAsList():
+    # stringOfCharacters = "abcd"
     stringOfCharacters = "abcdefghijklmnopqrstuvwxyz"
 
     listOfCharacters = []
@@ -26,15 +25,33 @@ def charactersAsList():
     for element in stringOfCharacters:
         listOfCharacters += element
 
-
-
     return listOfCharacters
 
-def printListAsString(listPassword):
+def numbersAsList():
+    stringOfNumbers = "0123456789"
+
+    listOfNumbers = []
+
+    for number in stringOfNumbers:
+        listOfNumbers += number
+
+    return listOfNumbers
+
+# Takes any list as input and returns it in form of string
+def ListToString(listPassword):
     password = ""
     for element in listPassword:
         password += element
     return password
+
+# Takes any string as input and returns it in form of string
+def stringToList(string):
+    lis = []
+
+    for element in string:
+        lis += element
+
+    return lis
 
 # # This function prints all the substrings of the string given as input
 # def substr(string):
@@ -47,8 +64,8 @@ def printListAsString(listPassword):
 #                 print(stringOfCharacters[k], end="")
 #             print()
 
-
-def findThisPass(stringPassword):
+# Using permutations to check all possible ways of arranging the characters
+def findThisPassLetters(stringPassword):
     listPassword = []
     listOfCharacters = charactersAsList()
     #
@@ -58,26 +75,57 @@ def findThisPass(stringPassword):
     perms = permutations(listOfCharacters, int(len(stringPassword)))
 
     for i in list(perms):
+        # Enable sleep to see whats hapening
         time.sleep(0.001)
         if functools.reduce(lambda i, j : i and j, map(lambda m, k: m == k, i, listPassword), True) :
-        # if Counter(i) == Counter(listPassword):
+        # if Counter(i) == Counter(listPassword):  # This didn't check the order of elements in list
             print(i)
             break
         else:
             print(i)
 
 
-    # checkedPassword = []
-    # while checkedPassword != listPassword:
-    #     checkedPassword = []
-    #     for i in range(0, int(len(listPassword))-1):
-    #         for j in range(0, int(len(listOfCharacters))-1):
-    #             checkedPassword[i] = listOfCharacters[j]
-    #
-    #     printListAsString(checkedPassword)
+def findThisPassNumbers(stringPassword):
+    listPassword = []
+    listOfNumbers = numbersAsList()
 
+    for element in stringPassword:
+        listPassword += element
+
+    perms = permutations(listOfNumbers, int(len(stringPassword)))
+
+    for i in list(perms):
+        # Enable sleep to see whats hapening
+        time.sleep(0.001)
+        if functools.reduce(lambda i, j : i and j, map(lambda m, k: m == k, i, listPassword), True) :
+        # if Counter(i) == Counter(listPassword):  # This didn't check the order of elements in list
+            print(i)
+            break
+        else:
+            print(i)
+
+# Main loop
 if __name__ == "__main__":
 
     password = input("Enter password : ")
 
-    findThisPass(password)
+    passwordAsList = stringToList(password)
+
+    passHasInt = False
+
+    for element in passwordAsList:
+        i = 0
+        if passwordAsList[i] in "0123456789":
+            passHasInt = True
+            i += 1
+
+    if passHasInt:
+        findThisPassNumbers(password)
+    elif not passHasInt:
+        findThisPassLetters(password)
+    else:
+        print("Sorry can't crack this one.")
+
+    # functions used to find the password
+    # findThisPassNumbers(password)
+    # findThisPassLetters(password)
